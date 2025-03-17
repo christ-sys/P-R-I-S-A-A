4eb82d07773fcab1f1c600705049107c09c2e298
Ichan <christremigio6@gmail.com>
2025-03-17 18:37:29 +0800

    logs

--- PrisaaAttendance/Form1.Designer.cs
+++ PrisaaAttendance/Form1.Designer.cs
@@ -30,28 +30,28 @@
             this.txtQrContent = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.PrisaaLogo = new System.Windows.Forms.PictureBox();
             this.lblVerified = new System.Windows.Forms.Label();
             this.lblName = new System.Windows.Forms.Label();
-            this.lblDate = new System.Windows.Forms.Label();
             this.lblTime = new System.Windows.Forms.Label();
-            this.timerTimeRef = new System.Windows.Forms.Timer(this.components);
-            this.timerRefresh = new System.Windows.Forms.Timer(this.components);
-            this.label2 = new System.Windows.Forms.Label();
-            this.PrisaaLogo = new System.Windows.Forms.PictureBox();
+            this.lblDate = new System.Windows.Forms.Label();
             this.userScrn = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.btnRegister = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerTimeRef = new System.Windows.Forms.Timer(this.components);
+            this.timerRefresh = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
-            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PrisaaLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.userScrn)).BeginInit();
+            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnStart
@@ -102,6 +102,28 @@
             this.panel1.Size = new System.Drawing.Size(860, 44);
             this.panel1.TabIndex = 5;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightSteelBlue;
+            this.label2.Location = new System.Drawing.Point(54, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(320, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "University Of Cagayan Valley";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox1.Image = global::PrisaaAttendance.Properties.Resources._201666_2256_43793_copy;
+            this.pictureBox1.Location = new System.Drawing.Point(6, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
             // panel2
             // 
             this.panel2.Controls.Add(this.PrisaaLogo);
@@ -118,20 +140,18 @@
             this.panel2.Size = new System.Drawing.Size(860, 406);
             this.panel2.TabIndex = 6;
             // 
-            // panel3
+            // PrisaaLogo
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.pictureBox2);
-            this.panel3.Controls.Add(this.btnStart);
-            this.panel3.Controls.Add(this.cmbCamera);
-            this.panel3.Controls.Add(this.btnRegister);
-            this.panel3.Controls.Add(this.label1);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(399, 406);
-            this.panel3.TabIndex = 10;
+            this.PrisaaLogo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PrisaaLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PrisaaLogo.Image = global::PrisaaAttendance.Properties.Resources.PrisaaBanner;
+            this.PrisaaLogo.Location = new System.Drawing.Point(532, 13);
+            this.PrisaaLogo.MaximumSize = new System.Drawing.Size(300, 200);
+            this.PrisaaLogo.Name = "PrisaaLogo";
+            this.PrisaaLogo.Size = new System.Drawing.Size(192, 124);
+            this.PrisaaLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PrisaaLogo.TabIndex = 11;
+            this.PrisaaLogo.TabStop = false;
             // 
             // lblVerified
             // 
@@ -160,17 +180,6 @@
             this.lblName.Text = "Welcome";
             this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // lblDate
-            // 
-            this.lblDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDate.Location = new System.Drawing.Point(420, 300);
-            this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(388, 37);
-            this.lblDate.TabIndex = 6;
-            this.lblDate.Text = "September 11,2024";
-            // 
             // lblTime
             // 
             this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
@@ -182,38 +191,16 @@
             this.lblTime.TabIndex = 5;
             this.lblTime.Text = "00:00:00";
             // 
-            // timerTimeRef
+            // lblDate
             // 
-            this.timerTimeRef.Tick += new System.EventHandler(this.timerTimeRef_Tick);
-            // 
-            // timerRefresh
-            // 
-            this.timerRefresh.Interval = 10000;
-            this.timerRefresh.Tick += new System.EventHandler(this.timerRefresh_Tick);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.LightSteelBlue;
-            this.label2.Location = new System.Drawing.Point(54, 5);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(320, 32);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "University Of Cagayan Valley";
-            // 
-            // PrisaaLogo
-            // 
-            this.PrisaaLogo.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.PrisaaLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.PrisaaLogo.Image = global::PrisaaAttendance.Properties.Resources.PrisaaBanner;
-            this.PrisaaLogo.Location = new System.Drawing.Point(532, 13);
-            this.PrisaaLogo.MaximumSize = new System.Drawing.Size(300, 200);
-            this.PrisaaLogo.Name = "PrisaaLogo";
-            this.PrisaaLogo.Size = new System.Drawing.Size(192, 124);
-            this.PrisaaLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.PrisaaLogo.TabIndex = 11;
-            this.PrisaaLogo.TabStop = false;
+            this.lblDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(420, 300);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(388, 37);
+            this.lblDate.TabIndex = 6;
+            this.lblDate.Text = "September 11,2024";
             // 
             // userScrn
             // 
@@ -229,6 +216,21 @@
             this.userScrn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.userScrn.TabIndex = 0;
             this.userScrn.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Controls.Add(this.btnStart);
+            this.panel3.Controls.Add(this.cmbCamera);
+            this.panel3.Controls.Add(this.btnRegister);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(399, 406);
+            this.panel3.TabIndex = 10;
             // 
             // pictureBox2
             // 
@@ -267,16 +269,14 @@
             this.label1.TabIndex = 3;
             this.label1.Text = ".";
             // 
-            // pictureBox1
+            // timerTimeRef
             // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.pictureBox1.Image = global::PrisaaAttendance.Properties.Resources._201666_2256_43793_copy;
-            this.pictureBox1.Location = new System.Drawing.Point(6, -2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(42, 50);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 12;
-            this.pictureBox1.TabStop = false;
+            this.timerTimeRef.Tick += new System.EventHandler(this.timerTimeRef_Tick);
+            // 
+            // timerRefresh
+            // 
+            this.timerRefresh.Interval = 10000;
+            this.timerRefresh.Tick += new System.EventHandler(this.timerRefresh_Tick);
             // 
             // FrmMain
             // 
@@ -294,14 +294,14 @@
             this.Load += new System.EventHandler(this.FrmMain_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.PrisaaLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.userScrn)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
