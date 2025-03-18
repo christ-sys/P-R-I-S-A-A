b1765f780eca584429d8162091907d755313de63
Ichan <christremigio6@gmail.com>
2025-03-18 09:48:04 +0800

    Directory and Bg enhancement

--- PrisaaAttendance/Properties/Resources.Designer.cs
+++ PrisaaAttendance/Properties/Resources.Designer.cs
@@ -163,6 +163,26 @@ namespace PrisaaAttendance.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap PrisaaBanner_bg {
+            get {
+                object obj = ResourceManager.GetObject("PrisaaBanner bg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap PrisaaBanner_bg1 {
+            get {
+                object obj = ResourceManager.GetObject("PrisaaBanner bg1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Web {
             get {
                 object obj = ResourceManager.GetObject("Web", resourceCulture);
